90a3530ff8a41eb4b1033ed6e748c13857c739d5
petermagalong <43397351+petermagalong@users.noreply.github.com>
2021-11-29 07:01:45 +0800

    Mouse move

--- src/App.js
+++ src/App.js
@@ -3,15 +3,28 @@ import { useEffect, useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
   const [isOn, setIson] = useState(false);
-
-
+  const [mousePosition, setMousePosition]= useState({x: null, y:null});
+  const [status, setStatus] = useState(navigator.onLine)
   /*
   Note :
   Use Effect run affect after every render 
   */ 
   useEffect(() => {
-    document.title = `Youcliked ${count} times`
-  })
+    document.title = `Youcliked ${count} times`;
+    window.addEventListener('mousemove',handleMouseMove)
+
+    return () => {
+      window.removeEventListener('mousemove',handleMouseMove)
+    }
+  },
+  [count]);
+
+  const handleMouseMove = event => {
+    setMousePosition({
+      x: event.pageX,
+      y: event.pageY
+    })
+  }
   const incrementCount = () =>{
     setCount(previousCount => previousCount + 1);
     console.log(setCount)
@@ -24,8 +37,6 @@ function App() {
   <>
   <h2>Counter</h2>
   <button onClick={incrementCount}>I was Click {count}</button>
-
-
   <h2>Toggle Light</h2>
   <div
     style={{
@@ -34,9 +45,11 @@ function App() {
       background: isOn ? "yellow" : "grey"
     }}
     onClick={toggleLight}>
-
-    
   </div>
+
+  <h2>Mouse Position</h2>
+  {JSON.stringify(mousePosition,null,2)}
+  <br />
   </>
   );
 }
